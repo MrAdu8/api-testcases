85d2bd1de1175a5bfa9e342159154f480fa51855
MrAdu8 <125743120+MrAdu8@users.noreply.github.com>
2023-12-31 20:04:12 +0530

    basic commad is done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) =>{
+    res.send("This is basic express api");
+});
+
+const start = async () =>{
+    try {
+        app.listen(PORT, ()=>{
+            console.log(`You are connected to ${PORT} port`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
