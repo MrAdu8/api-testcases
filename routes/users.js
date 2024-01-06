c6ace5cd0d39346938156849bc666053e81a288e
MrAdu8 <125743120+MrAdu8@users.noreply.github.com>
2024-01-06 14:16:11 +0530

    api connected to database

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('../database');
+const connection = require('../database');
 
+
+router.get('/hi', (req, res)=> {
+    res.send("api is working correctly :)");
+});
 
 router.get('/', (req, res)=> {
-    res.send("api is working correctly :)");
+    const getalluser = "SELECT * FROM `users`";
+
+    connection.query(getalluser, function(err, result) {
+        if (err) {
+            console.log(err);
+            res.send("Unable to get Users Data");
+        } else {
+            res.send(result);
+        }
+    });
 });
 
 module.exports = router;
