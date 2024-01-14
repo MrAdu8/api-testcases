b138dc1e68931a3d6625bd3712b9c4d939029605
MrAdu8 <125743120+MrAdu8@users.noreply.github.com>
2024-01-15 00:18:52 +0530

    hobby api created

--- routes/hobbies.js
+++ routes/hobbies.js
@@ -16,7 +16,54 @@ router.get('/', async(req, res) =>{
             res.status(200).json(result);
         }
     } catch (err) {
-        res.status(500).json({error: 'failed to get all User'});
+        res.status(500).json({error: 'failed to get all hobbies'});
+    }
+});
+
+// add new hobby
+router.post('/', async(req, res) => {
+    try {
+        const {hobby} = req.body;
+
+        if (!hobby) {
+            throw new Error("hobby must be mandatory");
+        }
+
+        const SQL = "INSERT INTO hobbies SET ?";
+
+        const result = await connection.query(SQL, {hobby: hobby});
+
+        if (result.effectedRows === 0) {
+            res.status(400).json({error: "Not found"});
+        } else {
+            console.log(result);
+            res.status(200).json(result);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Unable to update hobby'});
+    }
+})
+
+// update existing hobby
+router.put('/:hobby_id', async(req, res)=>{
+    try{
+        const hobby_id = req.params.hobby_id;
+        const {hobby} = req.body;
+        const SQL = 'UPDATE hobbies SET name=? WHERE hobby_id=?';
+
+        const result = await connection.query(SQL, [hobby_id]);
+
+        if(result.affectedRows === 0){
+            console.log(result);
+            res.status(400).json({error: "hobby cannot be updated"});
+        }else{
+            res.status(200).json(result);
+        }
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: "Failed to Update hobby"});
     }
 });
 
